eeca8d30b48408a07b4d7c5d76ce9a6459b386e8
Diogorot-2 <diogosilva.ds338@gmail.com>
2025-08-21 11:06:52 -0300

    atualizando pom.xml e Ninjamodel

--- src/main/java/dev/InovaXtech/CadastroDeNinjas/Ninja/NinjaModel.java
+++ src/main/java/dev/InovaXtech/CadastroDeNinjas/Ninja/NinjaModel.java
@@ -1,13 +1,28 @@
 package dev.InovaXtech.CadastroDeNinjas.Ninja;
 
 import java.util.List;
-
 import dev.InovaXtech.CadastroDeNinjas.missoes.missoes;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "tb_cadastro")
 
 public class NinjaModel {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	
 	private String Nome;
@@ -22,49 +37,6 @@ public class NinjaModel {
 	
 	private missoes Missoes;
 	
-	
-	public NinjaModel() {
-		
-	}
-
-	public NinjaModel(String nome, String email, int idade) {
-		
-		this.Nome = nome;
-		this.email = email;
-		this.idade = idade;
-	}
-
-	public String getNome() {
-		return Nome;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public int getIdade() {
-		return idade;
-	}
-
-	public void setNome(String nome) {
-		Nome = nome;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public void setIdade(int idade) {
-		this.idade = idade;
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
 	
 	
 	
